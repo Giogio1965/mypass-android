b90c603de7f2b3abcc1d9fa7aaa68786ba6e2c25
Giogio1965 <157477255+Giogio1965@users.noreply.github.com>
2026-03-18 09:59:06 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.giovanni.mypass',
+  appName: 'MyPass',
+  webDir: 'dist'
+};
+
+export default config;
